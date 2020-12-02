7c150e9b69b12d9a70c6629cbe56790e8b6e1744
Chathurani <csenevirathna@gmail.com>
2020-12-02 20:17:00 +0000

    add modules

---
+++ modules/globalvars.py
@@ -0,0 +1,14 @@
+'''
+This code is authored by Chathurani Senevirathna
+
+Also this is an implementaion for the paper submission for the journal Entropy.
+
+This contains the global varibales used in the initialization.py, analyze.py and infcascades.py
+'''
+
+
+relationshipTypes=['creationTocreation', 'creationTocontribution', 'creationTosharing', 
+                   'contributionTocreation', 'contributionTocontribution', 'contributionTosharing', 
+                   'sharingTocreation', 'sharingTocontribution', 'sharingTosharing']
+platforms = ["GitHub","Twitter"]
+communities = ["Crypto","CVE"]
